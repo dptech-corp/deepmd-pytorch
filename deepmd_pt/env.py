efb579a1079f4953c987582485aa6feff7b9277a
hangrui bi <hangruibi@outlook.com>
2023-04-12 18:16:27 +0800

    fixed set_device

--- deepmd_pt/env.py
+++ deepmd_pt/env.py
@@ -4,10 +4,10 @@ import torch
 GLOBAL_NP_FLOAT_PRECISION = np.float64
 GLOBAL_PT_FLOAT_PRECISION = torch.float64
 GLOBAL_ENER_FLOAT_PRECISION = np.float64
-DEVICE = torch.device(0)
-PREPROCESS_DEVICE = torch.device(0)
+DEVICE = torch.device('cuda')
+PREPROCESS_DEVICE = torch.device('cuda')
 JIT = True
 CACHE_PER_SYS = 5 # keep at most so many sets per sys in memory
 TEST_CONFIG = 'tests/water/se_e2_a.json'
-WORLD_SIZE = 1
+WORLD_SIZE = 2
 ENERGY_BIAS_TRAINABLE = True
