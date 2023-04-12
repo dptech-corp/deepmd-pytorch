8e16b6d2025d30f0a3ead0c5f1e4eb31057f3218
zjgemi <liuxin_zijian@163.com>
2023-04-12 10:04:20 +0800

    get device from env var
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- deepmd_pt/env.py
+++ deepmd_pt/env.py
@@ -1,11 +1,18 @@
 import numpy as np
+import os
 import torch
 
 GLOBAL_NP_FLOAT_PRECISION = np.float64
 GLOBAL_PT_FLOAT_PRECISION = torch.float64
 GLOBAL_ENER_FLOAT_PRECISION = np.float64
-DEVICE = torch.device('cpu')
-PREPROCESS_DEVICE = torch.device('cpu')
+if os.environ.get("DEVICE") == "cpu":
+    DEVICE = torch.device('cpu')
+else:
+    DEVICE = torch.device(0)
+if os.environ.get("PREPROCESS_DEVICE") == "cpu":
+    PREPROCESS_DEVICE = torch.device('cpu')
+else:
+    PREPROCESS_DEVICE = torch.device(0)
 JIT = True
 CACHE_PER_SYS = 5 # keep at most so many sets per sys in memory
 TEST_CONFIG = 'tests/water/se_e2_a.json'
