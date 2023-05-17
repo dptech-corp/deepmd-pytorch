8cff0bfd8f6293cf1068ae9bae6b3120af71bf4b
qin2xue3jian4 <471022852@qq.com>
2023-05-18 00:22:34 +0800

    Update finetune.py

--- deepmd_pt/utils/finetune.py
+++ deepmd_pt/utils/finetune.py
@@ -10,7 +10,7 @@ def get_model_params(ckpt, finetune, config):
     """
     model_params = config['model']
     model_params["resuming"] = False
-    if ((ckpt is not None) or (finetune is not None)):
+    if finetune is not None:
         origin_model = finetune if finetune is not None else ckpt
         state_dict = torch.load(origin_model)
         if 'other_info' in state_dict:
@@ -20,6 +20,8 @@ def get_model_params(ckpt, finetune, config):
             assert set(new_type_map).issubset(old_type_map), "Only support for smaller type map when finetuning or resuming."
             model_params = last_model_params
             model_params["new_type_map"] = new_type_map
+        model_params["resuming"] = True
+    elif ckpt is not None:
         model_params["resuming"] = True
     config['model'] = model_params
     return config
