fix bug of nnei normalization. add control of linear. better layer update

deepmd_pt/model/descriptor/se_uni.py
@@ -18,6 +18,11 @@ from .se_atten import analyze_descrpt
 mydtype = env.GLOBAL_PT_FLOAT_PRECISION
 mydev = env.DEVICE
 
+def torch_linear(*args, **kwargs):
+  return torch.nn.Linear(*args, **kwargs, dtype=mydtype, device=mydev)
+simple_linear  = SimpleLinear
+mylinear = simple_linear
+
 class Atten2Map(torch.nn.Module):
   def __init__(
       self,
@@ -29,7 +34,7 @@ class Atten2Map(torch.nn.Module):
     self.ni = ni
     self.nd = nd
     self.nh = nh
-    self.mapqk = torch.nn.Linear(ni, nd * 2 * nh, bias=False, dtype=mydtype, device=mydev)
+    self.mapqk = mylinear(ni, nd * 2 * nh, bias=False)
     
   def forward(
       self,
@@ -52,6 +57,7 @@ class Atten2Map(torch.nn.Module):
       h2, torch.transpose(h2, -1, -2)) / 3.**0.5
     # nb x nloc x nh x nnei x nnei
     ret = torch.softmax(g2qk, dim=-1) * h2h2t[:,:,None,:,:]
+    # ret = torch.softmax(g2qk, dim=-1)
     # nb x nloc x nnei x nnei x nh
     ret = torch.permute(ret, (0, 1, 3, 4, 2))
     return ret
@@ -66,8 +72,8 @@ class Atten2MultiHeadApply(torch.nn.Module):
     super(Atten2MultiHeadApply, self).__init__()
     self.ni = ni
     self.nh = nh
-    self.mapv = torch.nn.Linear(ni, ni * nh, bias=False, dtype=mydtype, device=mydev)
-    self.head_map = torch.nn.Linear(ni * nh, ni, dtype=mydtype, device=mydev)
+    self.mapv = mylinear(ni, ni * nh, bias=False)
+    self.head_map = mylinear(ni * nh, ni)
   
   def forward(
       self,
@@ -98,7 +104,7 @@ class Atten2EquiVarApply(torch.nn.Module):
     super(Atten2EquiVarApply, self).__init__()
     self.ni = ni
     self.nh = nh
-    self.head_map = torch.nn.Linear(nh, 1, dtype=mydtype, device=mydev)
+    self.head_map = mylinear(nh, 1)
     
   def forward(
       self,
@@ -119,6 +125,9 @@ class Atten2EquiVarApply(torch.nn.Module):
     # nf x nloc x nnei x 3
     return torch.squeeze(self.head_map(ret), dim=-1)
 
+
+def print_stat(aa, info=""):
+  print(info, torch.mean(aa), torch.std(aa))
 
 class DescrptSeUni(Descriptor):
   def __init__(
@@ -155,8 +164,9 @@ class DescrptSeUni(Descriptor):
     self.g1_hiddens = g1_hiddens
     self.g2_hiddens = g2_hiddens
     self.type_embd = TypeEmbedNet(self.ntypes, g1_hiddens[0])
-    self.g2_embd = torch.nn.Linear(1, g2_hiddens[0], dtype=mydtype, device=mydev)
+    self.g2_embd = mylinear(1, g2_hiddens[0])
     self.act = get_activation_fn(activation)
+    self.use_attn2 = attn2_nhead > 0 and attn2_hidden > 0
 
     cal_1_dim = lambda g1d, g2d, ax: g1d + g2d*ax
     g1_in_dims = [cal_1_dim(d1,d2,self.axis_dim) \
@@ -208,32 +218,21 @@ class DescrptSeUni(Descriptor):
     nlist_mask = (nlist != -1)
     masked_nlist_loc = nlist_loc * nlist_mask
 
-    # print(extended_coord.shape)
-    # print('nlist.loc', nlist_loc.shape)
-    # # nb x nloc x nnei x 4
-    # print('dmatrix', dmatrix.shape)
-    # print('diff', diff.shape)
-    # print(extended_coord)
-    # print('nlist', nlist[:,0,:])
-    # print('diff', diff[:,0,:,0])
-    # print('dmat', dmatrix[:,0,:,0])
-    # print(extended_coord[0,0,:] - extended_coord[0,1,:])
-    # print('ss', self.mean.shape, self.stddev.shape, atype)
-
     # nb x nloc x ng1
     g1 = (self.type_embd(atype))
     # nb x nloc x nnei x 1,  nb x nloc x nnei x 3
     g2, h2 = torch.split(dmatrix, [1, 3], dim=-1)
-    # print(g2.shape)
-    # print('xxxxx', torch.mean(g2[...,0], dim=(0,1)), torch.std(g2[...,0], dim=(0,1)))
+    # nb x nloc x nnei x ng2
     g2 = (self.g2_embd(g2))
-    # print(g2.shape)
-    # print('yyyyy', torch.mean(g2[...,0], dim=(0,1)), torch.std(g2[...,4], dim=(0,1)))
-
 
     for ll in range(self.nlayers):
       g1, g2, h2 = self._one_layer(
-        ll, g1, g2, h2, masked_nlist_loc, nlist_mask, ll!=self.nlayers-1)
+        ll, g1, g2, h2, 
+        masked_nlist_loc, 
+        nlist_mask, 
+        update_chnnl_2=(ll!=self.nlayers-1),
+        use_attn2=self.use_attn2,
+      )
 
     return g1, None, None
 
@@ -241,12 +240,12 @@ class DescrptSeUni(Descriptor):
       self,
       in_dims,
       out_dims,
-      include_bias: bool=True,
+      bias: bool=True,
   ):
     ret = []
     for ii, oo in zip(in_dims, out_dims):
       ret.append(
-        torch.nn.Linear(ii, oo, bias=include_bias, dtype=mydtype, device=mydev))
+        mylinear(ii, oo, bias=bias))
     return torch.nn.ModuleList(ret)
 
   def _one_layer(
@@ -258,6 +257,7 @@ class DescrptSeUni(Descriptor):
       nlist,    # nf x nloc x nnei
       nlist_mask,
       update_chnnl_2: bool=True,
+      use_attn2: bool=True,
   ):
     nb, nloc, nnei, _ = g2.shape
     assert (nb, nloc) == g1.shape[:2]
@@ -268,41 +268,54 @@ class DescrptSeUni(Descriptor):
 
     # g1 = self.lmg1[ll](g1)
     # g2 = self.lmg2[ll](g2)
-    
+
+    g2_update = [g2]
+    h2_update = [h2]
+    g1_update = [g1]
+
     if update_chnnl_2:
       # nb x nloc x nnei x ng2
       g2_1 = self.act(self.linear2[ll](g2))
-      # nb x nloc x nnei x nnei x nh
-      AA = self.attn2_map[ll](g2, h2)
-      # nb x nloc x nnei x ng2
-      g2_2 = self.attn2_mh_apply[ll](AA, g2)
-      # nb x nloc x nnei x nh2
-      h2_1 = self.attn2_ev_apply[ll](AA, h2)
-    # nb x nloc x ng1
-    g1m = torch.split(g1, self.axis_dim, dim=-1)[0]
+      g2_update.append(g2_1)
+      if use_attn2:
+        # nb x nloc x nnei x nnei x nh
+        AA = self.attn2_map[ll](g2, h2)
+        # nb x nloc x nnei x ng2
+        g2_2 = self.attn2_mh_apply[ll](AA, g2)
+        g2_update.append(g2_2)
+        # nb x nloc x nnei x nh2
+        h2_1 = self.attn2_ev_apply[ll](AA, h2)
+        h2_update.append(h2_1)
     # nb x nloc x 3 x ng2
-    # g1_13 = torch.einsum("bijp,bijz,bikz,bikq->bipq", g2m, h2, h2, g2)
     h2g2 = torch.matmul(
-      torch.transpose(h2, -1, -2), g2)
+      torch.transpose(h2, -1, -2), g2) / (float(nnei)**1)
     # nb x nloc x 3 x axis
     h2g2m = torch.split(h2g2, self.axis_dim, dim=-1)[0]
     # nb x nloc x axis x ng2
     g1_13 = torch.matmul(
-      torch.transpose(h2g2m, -1, -2), h2g2)    
+      torch.transpose(h2g2m, -1, -2), h2g2) / (float(3.)**1)
     # nb x nloc x (axisxng2)
     g1_13 = g1_13.view(nb, nloc, self.axis_dim*ng2)
     # nb x nloc x [ng1+(axisxng2)]
     g1_1 = self.act(self.linear1[ll](
       torch.cat((g1, g1_13), dim=-1)
     ))
+    g1_update.append(g1_1)
+
+    def list_update(update_list):
+      nitem = len(update_list)
+      uu = update_list[0]
+      for ii in range(1,nitem):
+        uu = uu + update_list[ii]
+      return uu / (float(nitem) ** 0.5)
 
     # update
     if update_chnnl_2:
-      g2_new = 1./(3.**0.5) * (g2 + g2_1 + g2_2)
-      h2_new = 1./(2.**0.5) * (h2 + h2_1)
+      g2_new = list_update(g2_update)
+      h2_new = list_update(h2_update)
     else:
       g2_new, h2_new = None, None
-    g1_new = 1./(2.**0.5) * (g1 + g1_1)
+    g1_new = list_update(g1_update)
     return g1_new, g2_new, h2_new
 
 

deepmd_pt/model/network/network.py
@@ -154,7 +154,7 @@ class SimpleLinear(torch.nn.Module):
                  stddev=1.,
                  use_timestep=False,
                  activate=None,
-                 include_bias: bool = True,
+                 bias: bool = True,
                  ):
         """Construct a linear layer.
 
@@ -172,7 +172,7 @@ class SimpleLinear(torch.nn.Module):
 
         self.matrix = torch.nn.Parameter(data=Tensor(num_in, num_out))
         torch.nn.init.normal_(self.matrix.data, std=stddev / np.sqrt(num_out + num_in))
-        if include_bias:
+        if bias:
           self.bias = torch.nn.Parameter(data=Tensor(1, num_out))
           torch.nn.init.normal_(self.bias.data, mean=bavg, std=stddev)
         else:
@@ -411,8 +411,8 @@ class GatedSelfAttetion(torch.nn.Module):
         else:
             self.scaling = temperature
         self.normalize = normalize
-        self.in_proj = SimpleLinear(embed_dim, hidden_dim * 3, bavg=0., stddev=1., use_timestep=False, include_bias=False)
-        self.out_proj = SimpleLinear(hidden_dim, embed_dim, bavg=0., stddev=1., use_timestep=False, include_bias=False)
+        self.in_proj = SimpleLinear(embed_dim, hidden_dim * 3, bavg=0., stddev=1., use_timestep=False, bias=False)
+        self.out_proj = SimpleLinear(hidden_dim, embed_dim, bavg=0., stddev=1., use_timestep=False, bias=False)
 
     def forward(self, query, nei_mask, input_r: Optional[torch.Tensor]=None):
         """