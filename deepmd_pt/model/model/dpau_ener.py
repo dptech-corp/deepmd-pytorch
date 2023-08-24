add auto diff check of force and virial computation. fix bug of virial

deepmd_pt/model/model/dpau_ener.py
@@ -82,7 +82,8 @@ class EnergyModelDPAUni(BaseModel):
         lst = torch.jit.annotate(List[Optional[torch.Tensor]], [faked_grad])
         extended_force = torch.autograd.grad([energy], [extended_coord], grad_outputs=lst, create_graph=True)[0]
         assert extended_force is not None
-        virial = -torch.transpose(extended_coord, 1, 2) @ extended_force
+        # virial = -torch.transpose(extended_coord, 1, 2) @ extended_force
+        virial = -torch.transpose(extended_force, 1, 2) @ extended_coord
         mapping = mapping.unsqueeze(-1).expand(-1, -1, 3)
         force = torch.zeros_like(coord)
         force = torch.scatter_reduce(force, 1, index=mapping, src=extended_force, reduce='sum')