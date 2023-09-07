5b94ee2e5878477748c5de180210eabb2b1c4061
Duo <50307526+iProzd@users.noreply.github.com>
2023-09-07 18:05:57 +0800

    try to slim the ut

--- tests/test_training.py
+++ tests/test_training.py
@@ -18,6 +18,7 @@ class TestDPTrain:
     def test_dp_train(self):
         trainer = get_trainer(deepcopy(self.config))
         trainer.run()
+        self.tearDown()
 
     def tearDown(self):
         for f in os.listdir("."):
@@ -35,38 +36,38 @@ class TestEnergyModelSeA(unittest.TestCase, TestDPTrain):
         with open(input_json, "r") as f:
             self.config = json.load(f)
         self.config["model"] = deepcopy(model_se_e2_a)
-        self.config["training"]["numb_steps"] = 10
-        self.config["training"]["save_freq"] = 10
+        self.config["training"]["numb_steps"] = 1
+        self.config["training"]["save_freq"] = 1
 
 
-class TestEnergyModelDPA1(unittest.TestCase, TestDPTrain):
-    def setUp(self):
-        input_json = "tests/water/se_atten.json"
-        with open(input_json, "r") as f:
-            self.config = json.load(f)
-        self.config["model"] = deepcopy(model_dpa1)
-        self.config["training"]["numb_steps"] = 10
-        self.config["training"]["save_freq"] = 10
-
-
-class TestEnergyModelDPAU(unittest.TestCase, TestDPTrain):
-    def setUp(self):
-        input_json = "tests/water/se_atten.json"
-        with open(input_json, "r") as f:
-            self.config = json.load(f)
-        self.config["model"] = deepcopy(model_dpau)
-        self.config["training"]["numb_steps"] = 10
-        self.config["training"]["save_freq"] = 10
-
-
-class TestEnergyModelHybrid(unittest.TestCase, TestDPTrain):
-    def setUp(self):
-        input_json = "tests/water/se_atten.json"
-        with open(input_json, "r") as f:
-            self.config = json.load(f)
-        self.config["model"] = deepcopy(model_hybrid)
-        self.config["training"]["numb_steps"] = 10
-        self.config["training"]["save_freq"] = 10
+# class TestEnergyModelDPA1(unittest.TestCase, TestDPTrain):
+#     def setUp(self):
+#         input_json = "tests/water/se_atten.json"
+#         with open(input_json, "r") as f:
+#             self.config = json.load(f)
+#         self.config["model"] = deepcopy(model_dpa1)
+#         self.config["training"]["numb_steps"] = 1
+#         self.config["training"]["save_freq"] = 1
+#
+#
+# class TestEnergyModelDPAU(unittest.TestCase, TestDPTrain):
+#     def setUp(self):
+#         input_json = "tests/water/se_atten.json"
+#         with open(input_json, "r") as f:
+#             self.config = json.load(f)
+#         self.config["model"] = deepcopy(model_dpau)
+#         self.config["training"]["numb_steps"] = 1
+#         self.config["training"]["save_freq"] = 1
+#
+#
+# class TestEnergyModelHybrid(unittest.TestCase, TestDPTrain):
+#     def setUp(self):
+#         input_json = "tests/water/se_atten.json"
+#         with open(input_json, "r") as f:
+#             self.config = json.load(f)
+#         self.config["model"] = deepcopy(model_hybrid)
+#         self.config["training"]["numb_steps"] = 1
+#         self.config["training"]["save_freq"] = 1
 
 
 if __name__ == '__main__':
