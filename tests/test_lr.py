c791d45601b16d87de7f62dbac2f0106a0548e95
Chengqian-Zhang <2000011006@stu.pku.edu.cn>
2023-12-04 17:11:36 +0000

    Add UT for polynomial decay

--- tests/test_lr.py
+++ tests/test_lr.py
@@ -7,7 +7,7 @@ tf.disable_eager_execution()
 
 from deepmd.utils import learning_rate
 
-from deepmd_pt.utils.learning_rate import LearningRateExp
+from deepmd_pt.utils.learning_rate import LearningRateExp, LearningRatePolynomial
 
 
 class TestLearningRate(unittest.TestCase):
@@ -39,6 +39,36 @@ class TestLearningRate(unittest.TestCase):
         my_vals = [my_lr.value(step_id) for step_id in range(self.stop_step) if step_id % self.decay_step != 0]
         self.assertTrue(np.allclose(base_vals, my_vals))
 
+
+class TestLearningRatePolynomial(unittest.TestCase):
+    def setUp(self):
+        self.start_lr = 2e-4
+        self.stop_lr = 1e-4
+        self.stop_steps = 10
+    
+    def test_power_one_1(self):
+        my_lr = LearningRatePolynomial(self.start_lr, self.stop_lr, self.stop_steps)
+        my_vals = [my_lr.value(step_id) for step_id in range(self.stop_steps+1)]
+        base_vals = [0.0002, 0.00019, 0.00018, 0.00017, 0.00016, 0.00015, 0.00014, 0.00013, 0.00012, 0.00011, 0.00010]
+        self.assertTrue(np.allclose(base_vals, my_vals))
+
+    def test_power_one_2(self):
+        my_lr = LearningRatePolynomial(self.start_lr, self.stop_lr, self.stop_steps, decay_steps=5)
+        my_vals = [my_lr.value(step_id) for step_id in range(self.stop_steps+1)]
+        base_vals = [0.0002, 0.00018, 0.00016, 0.00014, 0.00012, 0.00010, 0.00010, 0.00010, 0.00010, 0.00010, 0.00010]
+        self.assertTrue(np.allclose(base_vals, my_vals))
+    
+    def test_power_one_3(self):
+        my_lr = LearningRatePolynomial(self.start_lr, self.stop_lr, self.stop_steps, decay_steps=5, cycle=True)
+        my_vals = [my_lr.value(step_id) for step_id in range(self.stop_steps+1)]
+        base_vals = [0.0002, 0.00018, 0.00016, 0.00014, 0.00012, 0.00010, 0.00014, 0.00013, 0.00012, 0.00011, 0.00010]
+        self.assertTrue(np.allclose(base_vals, my_vals))
+    
+    def test_power_two(self):
+        my_lr = LearningRatePolynomial(self.start_lr, self.stop_lr, self.stop_steps, power=2)
+        my_vals = [my_lr.value(step_id) for step_id in range(self.stop_steps+1)]
+        base_vals = [0.0002, 0.000181, 0.000164, 0.000149, 0.000136, 0.000125, 0.000116, 0.000109, 0.000104, 0.000101, 0.00010]
+        self.assertTrue(np.allclose(base_vals, my_vals))
 
 if __name__ == '__main__':
     unittest.main()
